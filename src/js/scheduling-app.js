f4762d625fefdf895202f28e1a9668944b5684c8
censwteam <54615135+censwteam@users.noreply.github.com>
2020-04-27 15:20:27 +0530

    Update scheduling-app.js

--- src/js/scheduling-app.js
+++ src/js/scheduling-app.js
@@ -25,7 +25,7 @@ function slotSearch() {
 
   FHIR.oauth2.ready(function(smart) {
     // Query the FHIR server for Slots
-    smart.api.fetchAll({type: 'Appointment', query: slotParams}).then(
+    smart.api.fetchAll({type: 'Slot', query: slotParams}).then(
 
       // Display Appointment information if the call succeeded
       function(slots) {
