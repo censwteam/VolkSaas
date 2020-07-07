3fdbfc9368c8332686f73fb11fdaec8da65951c0
censwteam <54615135+censwteam@users.noreply.github.com>
2020-07-07 19:04:58 +0530

    Update Settings.js

--- ModelJS/Settings.js
+++ ModelJS/Settings.js
@@ -1,8 +1,8 @@
 ﻿//load
 var enumConfig =
 {
-    PATIENT_API_URL: "https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/",
-    PATIENT_INSURANCE_URL: "https://fhir-open.sandboxcerner.com/r4/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/",
+    PATIENT_API_URL: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/",
+    PATIENT_INSURANCE_URL: "https://fhir-ehr.sandboxcerner.com/r4/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/",
     PATIENT_RESOURCE_NAME: "Patient",
     COVERAGE_RESOURCE_NAME: "Coverage",
     OBSERVATION_RESOURCE_NAME: "Observation",
