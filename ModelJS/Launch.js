d3020d9847a56b4a63531acbdbccade4b6d3f942
censwteam <54615135+censwteam@users.noreply.github.com>
2020-07-08 19:19:22 +0530

    Update Launch.js

--- ModelJS/Launch.js
+++ ModelJS/Launch.js
@@ -167,7 +167,7 @@ function GetFundusPhotographyScheduledPatient(authToken) {
                 $('#loadingimage').show();
             },
             //url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Appointment?practitioner=" + practitionerID + "&date=ge" + fromFullFormat + "&date=lt" + toFullFormat  ,
-	    url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Slot?schedule.actor=Practitioner/" + practitionerID + "&start=ge" + fromFullFormat +"&date=lt" + toFullFormat + "&slot-type=http://snomed.info/sct|408443003",	
+	    url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Slot?slot-type=http://snomed.info/sct|408443003&start=ge" + fromFullFormat +"&date=lt" + toFullFormat ,	
             //url: "https://fhir-open.sandboxcerner.com/r4/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Appointment?date=ge" + fromFullFormat + "&date=lt" + toFullFormat + "&practitioner=" + practitionerID + "&code=http://snomed.info/sct%7C408443003" ,
             dataType: "json",
             //async: false,
