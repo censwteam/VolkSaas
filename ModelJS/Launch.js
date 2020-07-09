0cb93d38c0c3f9d481d976b473eb4e3700893e30
censwteam <54615135+censwteam@users.noreply.github.com>
2020-07-09 12:19:38 +0530

    Update Launch.js

--- ModelJS/Launch.js
+++ ModelJS/Launch.js
@@ -362,6 +362,7 @@ function GetFundusPhotographyScheduledPatient(authToken) {
 						    headers: {
                                                         Accept: "application/json+fhir",
                                                        // "Content-Type": "application/json+fhir",
+							Access-Control-Allow-Origin: "https://censwteam.github.io",    
 							"Authorization":"Bearer " + authToken
 							    //"Access-Control-Allow-Origin", "*"
                                                     }
@@ -523,6 +524,7 @@ function GetFundusPhotographyScheduledPatient(authToken) {
 						    headers: {
                                                         Accept: "application/json+fhir",
                                                         //"Content-Type": "application/json+fhir",
+							Access-Control-Allow-Origin: "https://censwteam.github.io",
 							"Authorization":"Bearer " + authToken    
 							    //"Access-Control-Allow-Origin", "*"
                                                     },
