7ab5fd9243f7e4e979b4275f430f4596c2bad228
censwteam <54615135+censwteam@users.noreply.github.com>
2020-07-13 11:10:28 +0530

    Update Launch.js

--- ModelJS/Launch.js
+++ ModelJS/Launch.js
@@ -96,7 +96,7 @@ function GetFundusPhotographyScheduledPatient(authToken) {
     HGB = ""; Cholesterol = ""; Subscriber=""; Beneficiary=""; CoverageStartDate=""; 
     CoverageEndDate=""; Payor=""; GroupName=""; GroupValue="";IOPLeft="";IOPRight="";Glaucoma="";
     if (practitionerID !== "" && fromDate !== "" && toDate !== "") {
-      //  console.log("url - " + "https://fhir-open.sandboxcerner.com/r4/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Appointment?date=ge" + fromFullFormat + "&date=lt" + toFullFormat + "&practitioner=" + practitionerID + "");
+        console.log("url - " + "https://fhir-open.sandboxcerner.com/r4/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Appointment?date=ge" + fromFullFormat + "&date=lt" + toFullFormat + "&practitioner=" + practitionerID + "");
         $.ajax({
             headers: {
                 Accept: "application/json+fhir",
