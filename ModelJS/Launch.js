4838056401ecd60e048f6222f8eaeb497bef85c0
censwteam <54615135+censwteam@users.noreply.github.com>
2020-07-29 18:43:38 +0530

    Update Launch.js

--- ModelJS/Launch.js
+++ ModelJS/Launch.js
@@ -919,12 +919,12 @@ function GetFundusPhotographyScheduledPatient(authToken) {
 														if(resourceInnerHeader == "verificationStatus")
 														{
 															console.log("Diabetic Retinopathy " + resourceItems.code.text);
-															if(resourceItems.code.text.toLowerCase() == "No retinopathy Type1")
+															if(resourceItems.code.text.toLowerCase() == "no retinopathy type1")
 															{
 																console.log("inside Noretinopathy-Type1");
 																DiabeticRetinopathy= "No retinopathy - Type1";	
 															}
-															if(resourceItems.code.text.toLowerCase() == "No retinopathy Type2")
+															if(resourceItems.code.text.toLowerCase() == "no retinopathy type2")
 															{
 																console.log("inside Noretinopathy-Type2");
 																DiabeticRetinopathy= "No retinopathy - Type2";
