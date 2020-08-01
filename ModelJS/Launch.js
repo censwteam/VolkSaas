37b3785fad257e5e95bd83e2041661c822538294
censwteam <54615135+censwteam@users.noreply.github.com>
2020-08-01 18:02:20 +0530

    Update Launch.js

--- ModelJS/Launch.js
+++ ModelJS/Launch.js
@@ -97,7 +97,7 @@ $.ajax({
 	beforeSend: function () {
 
 	},
-	url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition?patient=" + PatientID,
+	url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition?patient=" + patientId,
 	dataType: "json",
 	success: function (response) {
 	var stringfyJsonResponse = JSON.stringify(response);
