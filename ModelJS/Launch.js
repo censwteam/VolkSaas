ec01aef1541e4ad7f56a2934fd37678e86c96946
censwteam <54615135+censwteam@users.noreply.github.com>
2020-08-04 15:40:00 +0530

    Update Launch.js

--- ModelJS/Launch.js
+++ ModelJS/Launch.js
@@ -248,8 +248,8 @@ complete:  function () {
 						beforeSend: function () {
 							$('#loadingimage').show();
 						},
-					//url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition/" + DiabeticRetinopathyConditionId + "",	 
-					url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition/",	 
+					url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition/" + DiabeticRetinopathyConditionId + "",	 
+					//url: "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition/",	 
 					data: JSON.stringify(_json),
 						success: function (response) {
 					},
